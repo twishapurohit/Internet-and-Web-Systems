3012e441c0e398929f96c7d9112c46d1338e78c7
Twisha Purohit <twishapurohit@gmail.com>
2018-12-06 13:31:01 -0500

    Add files via upload

---
+++ Week 7/valid.js
@@ -0,0 +1,36 @@
+
+
+// Find all inputs on the DOM which are bound to a datalist via their list attribute.
+var inputs = document.querySelectorAll('input[list]');
+for (var i = 0; i < inputs.length; i++) {
+  // When the value of the input changes... 
+  inputs[i].addEventListener('change', function() {
+    var optionFound = false,
+      datalist = this.list;
+    // Determine whether an option exists with the current value of the input.
+    for (var j = 0; j < datalist.options.length; j++) {
+        if (this.value == datalist.options[j].value) {
+            optionFound = true;
+            break;
+        }
+    }
+    // use the setCustomValidity function of the Validation API
+    // to provide an user feedback if the value does not exist in the datalist
+    if (optionFound) {
+       this.setCustomValidity('');
+    } else {
+      this.setCustomValidity('This book does not exist in our database');
+    }
+  });
+}
+
+/*function listt(){
+for (var j = 0; j < datalist.options.length; j++) {
+    if (document.getElementById('txtList')[j] != datalist.options[j].value) 
+    {
+      alert("error");
+      return false;
+    }
+    return true;
+}
+}*/
